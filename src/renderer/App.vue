eb31a3b3a5d30e3944d55d2b5e085e4fbfbc91e4
WangJun <1103870790@qq.com>
2018-10-14 23:18:24 +0800

    init repo

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vinyl-editor'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
