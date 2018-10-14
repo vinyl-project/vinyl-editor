0b67186f309d12b88083eb145efb047b37ff3e86
WangJun <1103870790@qq.com>
2018-10-15 00:52:14 +0800

    update configs

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import axios from 'axios'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+Vue.use(Vuetify)
 
 import App from './App'
 import router from './router'
@@ -11,8 +15,8 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
-  router,
-  store,
-  template: '<App/>'
+    components: { App },
+    router,
+    store,
+    template: '<App/>'
 }).$mount('#app')
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -4,15 +4,14 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
-    },
-    {
-      path: '*',
-      redirect: '/'
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'main-page',
+            component: require('@/components/MainPage').default
+        },
+        {
+            path: '*',
+            redirect: '/'
+        }
+    ]
 })
